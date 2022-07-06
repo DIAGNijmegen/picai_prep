c1867db0b1725dc9f6694bfaea8b8ab52b0d506a
Joeran Bosma <joeran@bosma.co>
2022-07-06 15:37:20 +0200

    Bugfix

--- src/picai_prep/examples/dcm2mha/__init__.py
+++ src/picai_prep/examples/dcm2mha/__init__.py
@@ -11,3 +11,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+from picai_prep.examples.dcm2mha import (sample_archive,
+                                         sample_archive_inference)
+
+__all__ = [
+    "sample_archive",
+    "sample_archive_inference"
+]
