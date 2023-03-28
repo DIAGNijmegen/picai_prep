48180cdde45ac258eddebfa4000ec52ea1529e2e
Joeran Bosma <joeran@bosma.co>
2023-03-28 15:09:56 +0200

    dcm2mha type hinting and output filename (#46)
    - Type hint all functions in `dcm2ma.py`
    - Allow multiple mappings to point to the same output filename. This
    allows for example the following mapping:
    
    ```json
        "hbv": {
            "SeriesDescription": [
                "ep2d_diff"
            ],
            "SequenceName": [
                "ep_b1400"
            ]
        },
        "hbv:2": {
            "SeriesDescription": [
                "ep2d_diff.*CALC_BVAL"
            ]
        },
    ```
    
    Note: a `:` is not allowed in a filename anyway!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         long_description = fh.read()
 
     setuptools.setup(
-        version='2.1.5',
+        version='2.1.6',
         author_email='Joeran.Bosma@radboudumc.nl',
         long_description=long_description,
         long_description_content_type="text/markdown",
