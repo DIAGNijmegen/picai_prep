41086219e2f1ca70a06637a240b62dbd13e0b05f
Joeran Bosma <joeran@bosma.co>
2023-03-28 15:09:18 +0200

    Remove common prefixes from filenames (#45)
    I removed common prefixes from filenames. This allows to verify
    filenames like "1.2.86.1.dcm", ..., "1.2.86.12.dcm".
    
    I also removed `vdcms = [d.rsplit('.', 1)[0] for d in filenames]` since
    this is redundant in combination with the line after it: `vdcms =
    [int(''.join(c for c in str(fn) if c.isdigit())) for fn in filenames]`
    
    Finally, `True` is returned if filenames could not be verified for a
    particular series: only one slice or no number in the filename. This
    makes the default of checking for numbers more user friendly.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         long_description = fh.read()
 
     setuptools.setup(
-        version='2.1.4',
+        version='2.1.5',
         author_email='Joeran.Bosma@radboudumc.nl',
         long_description=long_description,
         long_description_content_type="text/markdown",
